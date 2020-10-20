f5bc60903b8c7064df8bcb07d7895c5ac5f1e076
bepro <jahwan.oh@bepro11.com>
2020-10-20 12:47:17 +0200

    score threshold

--- configs/_base_/models/cascade_rcnn_r50_fpn.py
+++ configs/_base_/models/cascade_rcnn_r50_fpn.py
@@ -178,6 +178,7 @@ test_cfg = dict(
         nms_thr=0.7,
         min_bbox_size=0),
     rcnn=dict(
-        score_thr=0.10,
+        #score_thr=0.10,
+        score_thr=0.50,
         nms=dict(type='nms', iou_threshold=0.60),
         max_per_img=100))
