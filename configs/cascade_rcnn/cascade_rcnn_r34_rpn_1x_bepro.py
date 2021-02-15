a754ab2049049a882ae1e887801be85cab055e1d
Dmitriy Khvan <dmitriy.khvan@bepro11.com>
2021-02-15 17:27:29 +0100

    adding resnet34 configs

---
+++ configs/cascade_rcnn/cascade_rcnn_r34_rpn_1x_bepro.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/cascade_rcnn_r34_f64_fpn.py',
+    '../_base_/datasets/bepro_detection.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/bepro_runtime.py'
+]
