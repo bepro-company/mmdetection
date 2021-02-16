3ef201c7d6f6aeceb6e51e6f458354a41871238d
Dmitriy Khvan <dmitriy.khvan@bepro11.com>
2021-02-16 17:49:03 +0100

    skiping 1 frame when detecting players

--- demo/mmdetection_demo.py
+++ demo/mmdetection_demo.py
@@ -42,8 +42,28 @@ def process_video_crcnn(frame_offset, frame_count, config_file, checkpoint_file,
 
     last_boxes = []
 
+    parity = False
+    if slice_start %2 != 0 and (slice_end-1) % 2 != 0: # odd - odd
+        parity = True
+    elif slice_start %2 == 0 and (slice_end-1) % 2 == 0: # even - even
+        parity = True
+
     for index in range(slice_start,slice_end):
         frame = video[index]
+
+        # skip frame
+        # note: if index of last frame in chunk is even, throw away odd indexes and vice versa
+        # frame of last index is needed for inter-chunk connection
+        if parity:
+            if (index-slice_start) % 2 != 0:
+                continue
+        else:
+            # instead of skipping 0 index skip 1 index
+            if (index-slice_start) != 0 and (index-slice_start) % 2 == 0:
+                continue
+            if (index-slice_start) == 1:
+                continue
+
         f_number = f_number + 1
         
         if frame is None:
