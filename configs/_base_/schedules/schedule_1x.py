20fdccb27a3e4eb69b3dc728f867cddc49bca64c
Dmitriy Khvan <dmitriy.khvan@bepro11.com>
2021-07-09 18:18:42 +0000

    changing scheduler

--- configs/_base_/schedules/schedule_1x.py
+++ configs/_base_/schedules/schedule_1x.py
@@ -1,12 +1,12 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.0012, momentum=0.96, weight_decay=0.0001)
 # optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
-    warmup_iters=500,
+    warmup_iters=1,
     warmup_ratio=0.001,
     step=[8, 11])
-runner = dict(type='EpochBasedRunner', max_epochs=15)
+runner = dict(type='EpochBasedRunner', max_epochs=16)
